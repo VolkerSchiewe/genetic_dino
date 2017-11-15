6ac3bd54dfd803e1b9b98c0a5d3a91884e18826c
Volker Schiewe <volker.s1994@gmail.com>
2017-11-15 13:28:13 +0100

    Using Babel to compile es6 and File watcher (#10)
    * babel to compile es6
    
    * update readme

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
-const path = require('path');
+var path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map'
 };
