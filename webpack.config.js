d96442a5890e8d696f339f363faf082b8038939a
Philipp Hager <philipp.konstantin.hager@gmail.com>
2017-11-15 15:15:31 +0100

    Ignore npm packages

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
             {
                 test: /\.js$/,
                 loader: 'babel-loader',
+                exclude: /(node_modules|bower_components)/,
                 query: {
                     presets: ['es2015']
                 }
