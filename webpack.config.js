467a2a654ffe4d335e773c07490af1928cd88488
Philipp Hager <philipp.konstantin.hager@gmail.com>
2017-11-20 23:00:14 +0100

    Remove bower_components from wepack config

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
             {
                 test: /\.js$/,
                 loader: 'babel-loader',
-                exclude: /(node_modules|bower_components)/,
+                exclude: /(node_modules)/,
                 query: {
                     presets: ['es2015']
                 }
