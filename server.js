88aa8a6e8e058f75f323651ef015cf9d6773a032
VolkerSchiewe <volker.s1994@gmail.com>
2018-01-25 21:30:27 +0100

    get port from env

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/'));
+const PORT = process.env.PORT || 5000;
 
-var PORT = 8080;
+app.use(express.static(__dirname + '/'));
 app.listen(PORT);
 console.log(`Server listening on ${PORT}\nQuit the server with ^C.`);
